767f9204d78d2a5a4de50dfefcce247bf5ea87fd
David <tiqtok@gmail.com>
2018-11-05 16:44:44 -0800

    fixes foreign key association mapping

--- db/migrate/20181105064815_create_todos.rb
+++ db/migrate/20181105064815_create_todos.rb
@@ -1,7 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[5.1]
   def change
     create_table :todos do |t|
-    	t.integer :todo_id
     	t.integer :user_id
     	t.string :description
     	t.integer :alloted_time_in_hours
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20181105064815) do
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
-    t.integer "todo_id"
     t.integer "user_id"
     t.string "description"
     t.integer "alloted_time_in_hours"
@@ -26,7 +25,6 @@ ActiveRecord::Schema.define(version: 20181105064815) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_id"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
