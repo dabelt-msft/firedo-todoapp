751fe7e956460fd8e17a0fade6e6516caa70fae3
David <tiqtok@gmail.com>
2018-11-07 21:17:53 -0800

    Updates session, error message, migration, and seeds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    @e = "Invalid credentials"
   end
 
   def create
@@ -9,10 +10,10 @@ class SessionsController < ApplicationController
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       log_in @user
-      redirect_to @user
+      redirect_to "/users/#{@user.id}/todos"
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to new_users_session_path
+      redirect_to new_session_path flash[:error] = "Invalid Credentials"
     end
   end
 
