c92b0236c5884662c214754424c652ee828c95a6
dawit <davucan@gmail.com>
2018-11-09 23:16:22 -0800

    add todo show page, add new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,28 @@ class TodosController < ApplicationController
 			@todos = current_user.todos
 		end
 	end
+
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		t = Todo.new(user_id: current_user.id)
+		t.update(todo_params)
+		t.save
+		redirect_to "/users/#{current_user.id}/todos"
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def delete
+	end
+
+	def todo_params
+    	params.require(:todo).permit(:nominal_priority, :description, :allotted_time_in_hours, :dependencies)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions
+  resources :todos
 
   resources :users do
     resources :todos
