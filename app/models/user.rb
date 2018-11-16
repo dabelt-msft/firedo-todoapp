9615c37f9e46a40915add3c9ec7606aba0dc6ee4
David <tiqtok@gmail.com>
2018-11-15 20:47:26 -0800

    Resolves conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,33 @@ class User < ApplicationRecord
 	has_many :todos
 
 	accepts_nested_attributes_for :todos, reject_if: proc { |attributes| attributes['description'].blank? }
+
+	def total_nominal_priority 
+		self.todos.sum(:nominal_priority)
+	end
+
+	def total_allotted_time
+		self.todos.sum(:total_allotted_time_in_hours)
+	end
+
+	def update_priority
+		if self.todos.count == 0
+			return 1
+		else
+			return (1 - (self.todos. / items.totalHours)) * this.nominalPriority;
+
+
+		self.todos.each do |todo|
+			self.todos.calculated_priority = (1 - (todo.allotted_time_in_hours / total_allotted_time)) * todo.nominal_priority 
+		end
+	end
+
+	#     t.integer :nominal_priority
+    # 	t.integer :user_id
+    # 	t.string :description
+    # 	t.float :allotted_time_in_hours
+    #   t.string :dependencies
+    #   t.float :calculated_priority
+
+	
 end
